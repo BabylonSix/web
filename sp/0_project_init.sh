ce94ea5538a735d828a839d91f641b044df2d5d4
BabylonSix <bruyako@me.com>
2015-08-19 16:26:26 -0700

    fix meta bug

--- sp/0_project_init.sh
+++ sp/0_project_init.sh
@@ -24,7 +24,7 @@ doctype html
 html(lang='en')
 	head
 		meta(charset='utf-8')
-		meta(meta(http-equiv='X-UA-Compatible' content='IE=edge'))
+		meta(http-equiv='X-UA-Compatible' content='IE=edge')
 		meta(name='viewport' content='width=device-width, initial-scale=1')
 		meta(name='description' content='This is the information that appears in the google description for your page')
 		title
