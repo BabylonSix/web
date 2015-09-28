841118ab7d54d8ba9640d79f752028fe348ac609
BabylonSix <bruyako@me.com>
2015-09-27 22:51:29 -0700

    rewrote the kp function to kill multiple ports

--- web-commands.sh
+++ web-commands.sh
@@ -73,3 +73,27 @@ function server() {
 	open "http://localhost:${port}/"
 	python SimpleHTTPServer "$port"
 }
+
+
+
+# kill ports
+function kp() {
+	# if number of entered arguments is 0
+	if [[ $# -eq 0 ]]; then
+		echo 'Error: No parameters entered'
+	fi
+
+	# if number of entered arguments is > 0
+	if [[ $# > 0 ]]; then
+
+		# for each port in the arguments
+		( # prevent global variables by running everything in a sub-shell
+			# for Port# entered passed by the user,
+			# add the following to pkPorts -i TCP:Port#
+			for pkPort in $@
+				do
+					lsof -i TCP:$pkPort | grep LISTEN | awk '{print $2}' | xargs kill -9
+				done
+		)
+	fi
+}
