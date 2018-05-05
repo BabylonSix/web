d8820d9858fd220679474aa44745b412cd175d0b
Von Bravo <babylon.six@me.com>
2018-05-04 19:49:06 -0700

    change: isolated all the error codes into their own file

--- sj/sjFunctionality.sh
+++ sj/sjFunctionality.sh
@@ -6,27 +6,17 @@ sjo() {
 			if [[ -a $1/.sj ]]; then         # and it's a sj project
 				cd $1 || exit; sjOpenProject;  # open the project
 			else                             # otherwise
-				sjError                        # Print Error
+				sjError; sjCantOpen            # Print Error
 			fi
 
 		else                               # if sjo was NOT given an argument
-			if [[ -a .sj ]]; then            # but the folder we're in is an sj project
+			if [[ -a .sj ]]; then            # but the folder we're in is a sj project
 				sjOpenProject                  # open the sj project
 			else                             # otherwise
-				sjError                        # Print Error
+				sjError; sjCantOpen            # Print Error
 			fi
 		fi
 	} # end sjLogic
-
-
-	sjError() {
-		# Print Error
-		print '\n${RED}ERROR:${NC}'
-		print '\n  This is not a simple javascipt project!'
-		print '\n  Run: ${RED}sj${NC} ${GREEN}<project-name>${NC} command to create a new project.'
-		print '\n  Existing projects have a hidden .sj identifier file.'
-	} # end sjError
-
 
 	sjOpenProject() { # start sjOpenProject
 		# open current directory in text editor
