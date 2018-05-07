de8df6e3519d3d2f656f632f0716dca3be0c24ad
Von Bravo <babylon.six@me.com>
2018-05-07 11:41:13 -0700

    change: separated commands
    sjb & sjo now live in their own files

--- web-path.sh
+++ web-path.sh
@@ -7,7 +7,8 @@
 # Simple Javascript Project Build
 . ~/bin/web/sj/sjError.sh
 . ~/bin/web/sj/sjContent.sh
-. ~/bin/web/sj/sjFunctionality.sh
 . ~/bin/web/sj/sj.sh
+. ~/bin/web/sj/sjo.sh
+. ~/bin/web/sj/sjb.sh
 . ~/bin/web/projects.sh
 . ~/bin/web/web-commands.sh
